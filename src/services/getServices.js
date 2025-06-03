7817cb4f082860b2a0f0b8d2f01e99817190ac6d
Jacob Tham <jacobtham3@gmail.com>
2025-06-03 15:34:17 +0800

    feat: get player service

---
+++ src/services/getServices.js
@@ -0,0 +1,27 @@
+let playername = "szoboszlai";
+let league = 39;
+
+async function getPlayers() {
+  const url = `https://api-football-v1.p.rapidapi.com/v3/players?league=${league}&search=${playername}`;
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      //   body: JSON.stringify(data),
+      headers: {
+        "X-RapidAPI-Key": `5c20702421msh8972a317aee15c4p1e393ajsncda94ceaaf98`,
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+    return json;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+getPlayers();
