3d8ee5b6743581e5451ac115c41cd30335a67f47
Jacob Tham <jacob@Jacobs-MacBook-Pro-2.local>
2025-06-08 16:40:21 +0800

    feat: add pre-render conditions to prevent page crash

--- src/components/Fixtures/Fixtures.jsx
+++ src/components/Fixtures/Fixtures.jsx
@@ -24,14 +24,33 @@ const Fixtures = () => {
   }, []);
 
   return (
-    <>
-      <div className={styles.fixturesTable}>
-        <div>
-          <strong>Fixtures</strong>
-          <pre>{JSON.stringify(fixturesData, null, 2)}</pre>
-        </div>
+    <div className={styles.fixturesTable}>
+      <div>
+        <strong>Fixtures</strong>
       </div>
-    </>
+      <div className={styles.fixtures}>
+        {fixturesData && fixturesData.length > 0 ? (
+          fixturesData.map((game) => (
+            <div className={styles.fixtureContainer}>
+              <div className={styles.teamFixtureDetails}>
+                <p>{game.teams.home.name}</p>
+                <img className={styles.teamlogo} src={game.teams.home.logo} />
+              </div>
+              <div className={styles.fixtureGeneralDetails}>Game Details</div>
+              <div className={styles.teamFixtureDetails}>
+                <p>{game.teams.away.name}</p>
+                <img className={styles.teamlogo} src={game.teams.away.logo} />
+              </div>
+            </div>
+          ))
+        ) : fixturesData === null ? (
+          <p>Loading fixtures...</p>
+        ) : (
+          <p>No fixtures data available.</p>
+        )}
+        <pre>{JSON.stringify(fixturesData, null, 2)}</pre>
+      </div>
+    </div>
   );
 };
 
