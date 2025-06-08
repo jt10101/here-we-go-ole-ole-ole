3d10cf698d62288ec1ef495fcf4d0339452a906d
Jacob Tham <jacob@Jacobs-MacBook-Pro-2.local>
2025-06-08 13:26:23 +0800

    feat: basic standings table format established

--- src/components/StandingsTable/Standings.jsx
+++ src/components/StandingsTable/Standings.jsx
@@ -19,10 +19,27 @@ const Standings = () => {
   return (
     <>
       <p>Standings</p>
+      <div className={styles.tableHeader}>Header</div>
       <div className={styles.singleRow}>
-        <div className={styles.nameLogo}>Logo and Name</div>
-        <div className={styles.teamStats}>Stats</div>
+        <div className={styles.nameLogo}>
+          1 -
+          <img
+            className={styles.logo}
+            src="https://media.api-sports.io/football/teams/41.png"
+          />
+          Liverpool
+        </div>
+        <div className={styles.teamStats}>
+          <div>
+            {standingData?.response[0].league.standings[0][0].all.played}
+          </div>
+          <div>
+            {standingData?.response[0].league.standings[0][0].goalsDiff}
+          </div>
+          <div>{standingData?.response[0].league.standings[0][0].points}</div>
+        </div>
       </div>
+      <pre>{JSON.stringify(standingData, null, 2)}</pre>
     </>
   );
 };
