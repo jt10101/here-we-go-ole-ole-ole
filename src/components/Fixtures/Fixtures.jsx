c7c68c7ab9dabd7233948bb373b13d4570bfee80
Jacob Tham <jacob@Jacobs-MacBook-Pro-2.local>
2025-06-08 16:16:32 +0800

    feat: fixtures data pulled

--- src/components/Fixtures/Fixtures.jsx
+++ src/components/Fixtures/Fixtures.jsx
@@ -1,7 +1,36 @@
+import styles from "./Fixtures.module.css";
+import { useState, useEffect } from "react";
+import { getFixtures } from "../../services/getServices";
+
 const Fixtures = () => {
+  const [fixturesData, setFixturesData] = useState(null);
+  useEffect(() => {
+    const getFixturesData = async () => {
+      try {
+        let data = await getFixtures();
+        setFixturesData(data);
+        if (data && data.response && Array.isArray(data.response)) {
+          setFixturesData(data.response);
+        } else {
+          console.error("error:", data);
+          setFixturesData([]);
+        }
+      } catch (error) {
+        console.error("error:", error);
+        setFixturesData([]);
+      }
+    };
+    getFixturesData();
+  }, []);
+
   return (
     <>
-      <p>Fixtures</p>
+      <div className={styles.fixturesTable}>
+        <div>
+          <strong>Fixtures</strong>
+          <pre>{JSON.stringify(fixturesData, null, 2)}</pre>
+        </div>
+      </div>
     </>
   );
 };
