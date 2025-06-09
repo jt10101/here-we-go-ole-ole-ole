36b164c08f79f19241da6d67d9134568f844007d
Jacob Tham <jacob@mac.lan>
2025-06-09 23:42:31 +0800

    end of day

--- src/components/PositionDetails/Goalkeeper.jsx
+++ src/components/PositionDetails/Goalkeeper.jsx
@@ -1,9 +1,53 @@
-const Goalkeeper = ({ pdata }) => {
+const Goalkeeper = ({ pdata, styles }) => {
+  const shotsOn = pdata?.statistics[0].shots.on;
+  const shotsTotal = pdata?.statistics[0].shots.total;
+  let shotsAccuracy = 0;
+  if (shotsOn === 0 || shotsTotal === 0) {
+    return 0;
+  }
+  shotsAccuracy = Math.round((shotsOn / shotsTotal) * 100);
+
+  let goalperGame = 0;
+  if (
+    pdata?.statistics[0].goals.total === 0 ||
+    pdata?.statistics[0].games.appearences === 0
+  ) {
+    goalperGame = 0;
+  }
+  goalperGame =
+    pdata?.statistics[0].goals.total / pdata?.statistics[0].games.appearences;
+
   return (
     <>
-      <li>I am a goalkeeper</li>
-      <li>Saves: {pdata?.statistics[0].goals.saves}</li>
-      <li>Conceded: {pdata?.statistics[0].goals.conceded}</li>
+      <div className={styles.positionName}>Goalkeeping</div>
+
+      <div className={styles.stats}>
+        <div>Goals Saved</div>
+        <div>
+          <strong>{pdata?.statistics[0].goals.saves}</strong>
+        </div>
+      </div>
+
+      <div className={styles.stats}>
+        <div>Penalities Saved</div>
+        <div>
+          <strong>{pdata?.statistics[0].penalty.saved}</strong>
+        </div>
+      </div>
+
+      <div className={styles.stats}>
+        <div>Goals Conceded</div>
+        <div>
+          <strong>{pdata?.statistics[0].goals.conceded}</strong>
+        </div>
+      </div>
+
+      <div className={styles.stats}>
+        <div>Passes</div>
+        <div>
+          <strong>{pdata?.statistics[0].passes.total}</strong>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/PositionDetails/Midfielder.jsx
+++ src/components/PositionDetails/Midfielder.jsx
@@ -1,10 +1,95 @@
-const Midfielder = ({ pdata }) => {
+const Midfielder = ({ pdata, styles }) => {
+  const shotsOn = pdata?.statistics[0].shots.on;
+  const shotsTotal = pdata?.statistics[0].shots.total;
+  let shotsAccuracy = 0;
+  if (shotsOn === 0 || shotsTotal === 0) {
+    return 0;
+  }
+  shotsAccuracy = Math.round((shotsOn / shotsTotal) * 100);
+
+  let goalperGame = 0;
+  if (
+    pdata?.statistics[0].goals.total === 0 ||
+    pdata?.statistics[0].games.appearences === 0
+  ) {
+    goalperGame = 0;
+  }
+  goalperGame =
+    pdata?.statistics[0].goals.total / pdata?.statistics[0].games.appearences;
+
   return (
     <>
-      <li>I am a midfielder</li>
-      <li>Tackles: {pdata?.statistics[0].tackles.total}</li>
-      <li>Blocks: {pdata?.statistics[0].tackles.blocks}</li>
-      <li>Interceptions: {pdata?.statistics[0].tackles.interceptions}</li>
+      <div className={styles.positionName}>Attack</div>
+
+      <div className={styles.stats}>
+        <div>Goals</div>
+        <div>
+          <strong>{pdata?.statistics[0].goals.total}</strong>
+        </div>
+      </div>
+
+      <div className={styles.stats}>
+        <div>Goals per game</div>
+        <div>
+          <strong>{goalperGame.toFixed(2)}</strong>
+        </div>
+      </div>
+
+      <div className={styles.stats}>
+        <div>Shots</div>
+        <div>
+          <strong>{shotsTotal}</strong>
+        </div>
+      </div>
+
+      <div className={styles.stats}>
+        <div>Shots on target</div>
+        <div>
+          <strong>{shotsOn}</strong>
+        </div>
+      </div>
+
+      <div className={styles.stats}>
+        <div>Shots accuracy %</div>
+        <div>
+          <strong>{shotsAccuracy.toFixed(0)}%</strong>
+        </div>
+      </div>
+
+      <div className={styles.stats}>
+        <div>Assists</div>
+        <div>
+          <strong>{pdata?.statistics[0].goals.assists}</strong>
+        </div>
+      </div>
+
+      <div className={styles.stats}>
+        <div>Duels Total</div>
+        <div>
+          <strong>{pdata?.statistics[0].duels.total}</strong>
+        </div>
+      </div>
+
+      <div className={styles.stats}>
+        <div>Duels Won</div>
+        <div>
+          <strong>{pdata?.statistics[0].duels.won}</strong>
+        </div>
+      </div>
+
+      <div className={styles.stats}>
+        <div>Dribbles</div>
+        <div>
+          <strong>{pdata?.statistics[0].dribbles.attempts}</strong>
+        </div>
+      </div>
+
+      <div className={styles.stats}>
+        <div>Dribble Success</div>
+        <div>
+          <strong>{pdata?.statistics[0].dribbles.success}</strong>
+        </div>
+      </div>
     </>
   );
 };
