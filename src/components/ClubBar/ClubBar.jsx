cac4ee4d2ba19cb1850188c7dedeab131b327624
Jacob Tham <jacob@Jacobs-MacBook-Pro-2.local>
2025-06-09 10:21:58 +0800

    feat: team bar display developed

--- src/components/ClubBar/ClubBar.jsx
+++ src/components/ClubBar/ClubBar.jsx
@@ -2,34 +2,34 @@ import styles from "./ClubBar.module.css";
 // images from https://brandlogos.net/series/2024-25-premier-league-teams-logos-vector
 
 const ClubBar = () => {
-  const logoArr = [
-    "https://i.imgur.com/mSltbvt.png", // Bournemouth
-    "https://i.imgur.com/taVnjf4.png", // Arsenal
-    "https://i.imgur.com/Pjwwmwj.png", // Aston Villa
-    "https://i.imgur.com/7LguGF5.png", // Brentford
-    "https://i.imgur.com/GYG6VMS.png", // Brighton
-    "https://i.imgur.com/Ld5UVqG.png", // Chelsea
-    "https://i.imgur.com/jgcqLGv.png", // Crystal Palace
-    "https://i.imgur.com/xMWyv8d.png", // Everton
-    "https://i.imgur.com/IIgU0aG.png", // Fulham
-    "https://i.imgur.com/tRxuMpL.png", // Ipswich Town
-    "https://i.imgur.com/VTMboF0.png", // Leicester City
-    "https://i.imgur.com/Rb6V8HX.png", // Liverpool
-    "https://i.imgur.com/Szx0Yh6.png", // Manchester City
-    "https://i.imgur.com/Nk3jYct.png", // Manchester United
-    "https://i.imgur.com/sPNb2Hf.png", // Newcastle United
-    "https://i.imgur.com/f8tMZFS.png", // Nottingham Forest
-    "https://i.imgur.com/m7xYJAa.png", // Southampton
-    "https://i.imgur.com/MTiPYUd.png", // Tottenham Hotspur
-    "https://i.imgur.com/pJwuL3e.png", // West Ham United
-    "https://i.imgur.com/UwwsQRD.png", // Wolverhampton Wanderers
+  const teamArr = [
+    ["https://i.imgur.com/mSltbvt.png", 35], // Bournemouth
+    ["https://i.imgur.com/taVnjf4.png", 42], // Arsenal
+    ["https://i.imgur.com/Pjwwmwj.png", 66], // Aston Villa
+    ["https://i.imgur.com/7LguGF5.png", 55], // Brentford
+    ["https://i.imgur.com/GYG6VMS.png", 51], // Brighton
+    ["https://i.imgur.com/Ld5UVqG.png", 49], // Chelsea
+    ["https://i.imgur.com/jgcqLGv.png", 52], // Crystal Palace
+    ["https://i.imgur.com/xMWyv8d.png", 45], // Everton
+    ["https://i.imgur.com/IIgU0aG.png", 36], // Fulham
+    ["https://i.imgur.com/tRxuMpL.png", 57], // Ipswich Town
+    ["https://i.imgur.com/VTMboF0.png", 46], // Leicester City
+    ["https://i.imgur.com/Rb6V8HX.png", 40], // Liverpool
+    ["https://i.imgur.com/Szx0Yh6.png", 50], // Manchester City
+    ["https://i.imgur.com/Nk3jYct.png", 33], // Manchester United
+    ["https://i.imgur.com/sPNb2Hf.png", 34], // Newcastle United
+    ["https://i.imgur.com/f8tMZFS.png", 65], // Nottingham Forest
+    ["https://i.imgur.com/m7xYJAa.png", 41], // Southampton
+    ["https://i.imgur.com/MTiPYUd.png", 47], // Tottenham Hotspur
+    ["https://i.imgur.com/pJwuL3e.png", 48], // West Ham United
+    ["https://i.imgur.com/UwwsQRD.png", 39], // Wolverhampton Wanderers
   ];
   return (
     <>
       <div className={styles.clubbar}>
-        {logoArr.map((team) => (
+        {teamArr.map((team) => (
           <div>
-            <img className={styles.clubLogo} src={team} />
+            <img className={styles.clubLogo} value={team[1]} src={team[0]} />
           </div>
         ))}
       </div>
